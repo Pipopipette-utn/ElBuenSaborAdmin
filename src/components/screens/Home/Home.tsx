5d9b406034e046057767c2b1fe53114c143d21d4
jazminRillo <jazmin.rillo@gmail.com>
2024-05-06 08:48:16 -0300

    Dashboard

--- src/components/screens/Home/Home.tsx
+++ src/components/screens/Home/Home.tsx
@@ -1,7 +1,109 @@
-import { GenericDoubleStack } from "../../ui/shared/GenericDoubleStack";
+import { Divider, Stack, ToggleButton, ToggleButtonGroup } from "@mui/material";
+import {
+	ContainerStack,
+	ReportStack,
+	StyledStack,
+} from "../../ui/styled/StyledStack";
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+import GroupsIcon from "@mui/icons-material/Groups";
+import DateRangeIcon from "@mui/icons-material/DateRange";
+import TodayIcon from "@mui/icons-material/Today";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import {
+	ReportAmount,
+	ReportSubtitle,
+	ReportTitle,
+} from "../../ui/styled/Typography";
+import { useState } from "react";
+import ProductsPieChart from "../../ui/charts/ProductsPieChart";
+
 export const Home = () => {
-	return <GenericDoubleStack>
-		<>a</>
-		<>a</>
-	</GenericDoubleStack>;
+	const [month, setMonth] = useState<string | null>("may");
+
+	const handleMonth = (
+		event: React.MouseEvent<HTMLElement>,
+		newMonth: string | null
+	) => {
+		setMonth(newMonth);
+	};
+
+	return (
+			<StyledStack height="90vh" spacing={2} sx={{borderRadius: 0}}>
+				<Stack direction="row" spacing={3} width="100%">
+					<ReportStack spacing={1} flexGrow={1}>
+						<CalendarTodayIcon fontSize="large" />
+						<ReportAmount>$0.000,00</ReportAmount>
+						Recaudado hoy
+					</ReportStack>
+					<ReportStack spacing={1} flexGrow={1}>
+						<DateRangeIcon fontSize="large" />
+						<ReportAmount>$00.000,00</ReportAmount>
+						Recaudado esta semana
+					</ReportStack>
+					<ReportStack spacing={1} flexGrow={1}>
+						<TodayIcon fontSize="large" />
+						<ReportAmount>$000.000,00</ReportAmount>
+						Recaudado este mes
+					</ReportStack>
+				</Stack>
+				<Stack direction="row" spacing={3} width="100%">
+					<Stack spacing={2} width="40%">
+						<ReportStack spacing={2} alignItems="center">
+							<ReportTitle>Ganancias</ReportTitle>
+							<Stack direction="row" spacing={2}>
+								<Stack alignItems="center">
+									<ReportSubtitle>Costos</ReportSubtitle>
+									<ReportAmount>$00.000,00</ReportAmount>
+								</Stack>
+								<Divider orientation="vertical" />
+								<Stack alignItems="center">
+									<ReportSubtitle>Ventas</ReportSubtitle>
+									<ReportAmount>$00.000,00</ReportAmount>
+								</Stack>
+							</Stack>
+							<ToggleButtonGroup value={month} exclusive onChange={handleMonth}>
+								<ToggleButton value="january">J</ToggleButton>
+								<ToggleButton value="february">F</ToggleButton>
+								<ToggleButton value="march">M</ToggleButton>
+								<ToggleButton value="april">A</ToggleButton>
+								<ToggleButton value="may">M</ToggleButton>
+								<ToggleButton value="june">J</ToggleButton>
+								<ToggleButton value="july">J</ToggleButton>
+								<ToggleButton value="agost">A</ToggleButton>
+								<ToggleButton value="september">S</ToggleButton>
+								<ToggleButton value="october">O</ToggleButton>
+								<ToggleButton value="november">N</ToggleButton>
+								<ToggleButton value="december">D</ToggleButton>
+							</ToggleButtonGroup>
+						</ReportStack>
+
+						<Stack direction="row" spacing={2.5} width="100%">
+							<ReportStack direction="row" spacing={2} alignItems="center">
+								<GroupsIcon fontSize="large" />
+								<Stack padding="0">
+									<ReportTitle sx={{ lineHeight: "20px" }}>
+										Nuevos clientes
+									</ReportTitle>
+									<ReportAmount fontWeight="bolder">10+</ReportAmount>
+								</Stack>
+							</ReportStack>
+							<ReportStack direction="row" spacing={2} alignItems="center">
+								<PeopleAltIcon fontSize="large" />
+								<Stack padding="0">
+									<ReportTitle sx={{ lineHeight: "20px" }}>
+										Empleados activos
+									</ReportTitle>
+									<ReportAmount fontWeight="bolder">8</ReportAmount>
+								</Stack>
+							</ReportStack>
+						</Stack>
+					</Stack>
+
+					<ReportStack spacing={-3} alignItems="center" width="60%">
+						<ReportTitle>Productos más vendidos</ReportTitle>
+						<ProductsPieChart />
+					</ReportStack>
+				</Stack>
+			</StyledStack>
+	);
 };
---
+++ src/components/ui/charts/ProductsPieChart.tsx
@@ -0,0 +1,56 @@
+import { PieChart } from "@mui/x-charts/PieChart";
+
+const data2 = [
+	{ label: "Bebidas", value: 400 },
+	{ label: "Hamburguesas", value: 300 },
+	{ label: "Pizzas", value: 300 },
+	{ label: "Postres", value: 200 },
+	{ label: "Entradas", value: 200 },
+];
+
+const data1 = [
+	{ label: "Burger simple", value: 100, color: "#3035c9" },
+	{ label: "Burger Messi", value: 100, color: "#eb2f05" },
+	{ label: "Burger doble", value: 50, color: "#5d19e6" },
+	{ label: "Pizza muzzarella", value: 300, color: "#9987ff" },
+	{ label: "Pizza napolitana", value: 80, color: "#9f59f0" },
+	{ label: "Coca cola", value: 40, color: "#FF5733" },
+	{ label: "Cerveza Quilmes", value: 150, color: "#ff7e70" },
+	{ label: "Papas fritas", value: 100, color: "#ffc2b3" },
+];
+const series = [
+	{
+		innerRadius: 0,
+		outerRadius: 80,
+		id: "series-1",
+		data: data1,
+	},
+	{
+		innerRadius: 100,
+		outerRadius: 120,
+		id: "series-2",
+		data: data2,
+	},
+];
+
+const ProductsPieChart = () => {
+	return (
+		<PieChart
+			series={[
+				{
+					data: data1,
+					innerRadius: 30,
+					outerRadius: 100,
+					paddingAngle: 5,
+					cornerRadius: 5,
+					startAngle: -90,
+					endAngle: 180,
+					cx: 150,
+					cy: 150,
+				},
+			]}
+		/>
+	);
+};
+
+export default ProductsPieChart;
