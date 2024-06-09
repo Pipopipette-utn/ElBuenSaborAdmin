435201f46f784e03d2f82b8eba9febfe14a51208
DamianAguero329 <aguerodamian15@gmail.com>
2024-06-09 01:03:41 -0300

    Bandejas

---
+++ src/components/screens/Bandejas/BandejaEntrada.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BandejaEntrada = () => {
+  return (
+    <div>BandejaEntrada</div>
+  )
+}
+
+export default BandejaEntrada
---
+++ src/components/screens/Bandejas/BandejaSalida.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BandejaSalida = () => {
+  return (
+    <div>BandejaSalida</div>
+  )
+}
+
+export default BandejaSalida
