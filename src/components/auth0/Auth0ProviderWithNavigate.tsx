bc337860dfe0b869bd5d2803acb7d2abed6991e4
jazminRillo <jazmin.rillo@gmail.com>
2024-06-12 21:10:21 -0300

    corrigiendo error auth0

--- src/components/auth0/Auth0ProviderWithNavigate.tsx
+++ src/components/auth0/Auth0ProviderWithNavigate.tsx
@@ -1,4 +1,4 @@
-import { AppState, Auth0Provider } from "@auth0/auth0-react";
+import { Auth0Provider } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
 type Props = {
@@ -13,8 +13,8 @@ export const Auth0ProviderWithNavigate = ({ children }: Props) => {
 	const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
 	const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
   
-	const onRedirectCallback = (appState: AppState | undefined) => {
-		navigate(appState?.returnTo || "/");
+	const onRedirectCallback = () => {
+		navigate("/");
 	};
 
 
